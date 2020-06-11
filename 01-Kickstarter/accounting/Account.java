fc727a5de93e3b3ecc32a7cb3824fc223e5a7541
David Gomez G <dgomezg@gmail.com>
2020-06-11 11:41:26 +0200

    chapter 01 Kickstarter for begginers

---
+++ 01-Kickstarter/accounting/Account.java
@@ -0,0 +1,21 @@
+package accounting;
+
+public class Account {
+
+   private String _; //INvalid >Java9+
+
+   private String _accountNumber; //Valid >java9+
+
+
+   public static void main(String... args) {
+      System.out.println("Hello 1 2 3 testing ....");
+
+   }
+
+
+   }
+
+
+}
+
+
