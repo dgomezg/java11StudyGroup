a6d9445b8690775c70345bff9c9160911643e07e
Daniel Sanz <daniel.sanz@liferay.com>
2020-07-09 14:32:22 +0200

    add usage tests in non-static context. Add instructions to use these clases (book does not explore this in such detail)

--- 11-Encapsulation/src/com/access/SubOuterSamePackage.java
+++ 11-Encapsulation/src/com/access/SubOuterSamePackage.java
@@ -2,18 +2,37 @@ package com.access;
 
 public class SubOuterSamePackage extends Outer {
     public static void main(String[] args) {
+        // This does not work in an static context
+        //Outer.InnerPublic  pub2 = new Outer.InnerPublic();
+
         Outer outer = new Outer();
         SubOuterSamePackage subOuter = new SubOuterSamePackage();
 
-        Outer.InnerDefault def = outer.new InnerDefault();
-        Outer.InnerPublic  pub = outer.new InnerPublic();
-        Outer.InnerProtected prot = outer.new InnerProtected();
-        Outer.InnerPrivate priv = outer.new InnerPrivate();
+        /* how to use: uncomment each line, see what IDE says and check */
 
-        SubOuterSamePackage.InnerDefault subdef = subOuter.new InnerDefault();
-        SubOuterSamePackage.InnerPublic  subpub = subOuter.new InnerPublic();
-        SubOuterSamePackage.InnerProtected subprot = subOuter.new InnerProtected();
-        SubOuterSamePackage.InnerPrivate subpriv = subOuter.new InnerPrivate();
+        //Outer.InnerDefault def = outer.new InnerDefault();
+        //Outer.InnerPublic  pub = outer.new InnerPublic();
+        //Outer.InnerProtected prot = outer.new InnerProtected();
+        //Outer.InnerPrivate priv = outer.new InnerPrivate();
 
+        //SubOuterSamePackage.InnerDefault subdef = subOuter.new InnerDefault();
+        //SubOuterSamePackage.InnerPublic  subpub = subOuter.new InnerPublic();
+        //SubOuterSamePackage.InnerProtected subprot = subOuter.new InnerProtected();
+        //SubOuterSamePackage.InnerPrivate subpriv = subOuter.new InnerPrivate();
+
+    }
+
+    public void buildOuters() {
+        //Outer.InnerDefault subdef = new Outer.InnerDefault();
+        //Outer.InnerPublic  subpub = new Outer.InnerPublic();
+        //Outer.InnerProtected subprot = new Outer.InnerProtected();
+        //Outer.InnerPrivate subpriv = new Outer.InnerPrivate();
+    }
+
+    public void buildSubOuters() {
+        //SubOuterSamePackage.InnerDefault subdef = new SubOuterSamePackage.InnerDefault();
+        //SubOuterSamePackage.InnerPublic  subpub = new SubOuterSamePackage.InnerPublic();
+        //SubOuterSamePackage.InnerProtected subprot = new SubOuterSamePackage.InnerProtected();
+        //SubOuterSamePackage.InnerPrivate subpriv = new SubOuterSamePackage.InnerPrivate();
     }
 }
